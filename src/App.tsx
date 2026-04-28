9dca8ab6aba54a14a3bd54a4123255437af0bfe1
Manish Joshi <manishjoshi705572@gmail.com>
2026-04-28 23:54:13 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div style={{fontSize : "32px" , padding : "55px"}}>
+      Hi There I am Manish Joshi
+      <br />
+      <br />
+      <br />
+      This page is for pushing into the production
+
+    </div>
+  )
+}
+
+export default App
