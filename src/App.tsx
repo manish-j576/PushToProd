71683345a47b443a0d0d0681a8dc134099607d18
manish-j576 <72204361+manish-j576@users.noreply.github.com>
2026-04-29 00:02:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ function App() {
       <br />
       <br />
       <br />
+      I am going to earn 84LPA
+      <br />
+      <br />
+      <br />
       This page is for pushing into the production
 
     </div>
