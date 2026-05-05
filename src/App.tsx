4c291023bc32e36d884c231fe42960d93076c04b
Manish Joshi <manishjoshi705572@gmail.com>
2026-05-05 19:47:27 +0530

    Changes to profile photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 
 function App() {
   return (
-    <div style={{fontSize : "32px" , padding : "55px"}}>
-      Hi There I am Manish 
+    <div style={{ fontSize: "32px", padding: "55px" }}>
+      Hi There I am Manish
       <br />
       we will see
+      <img src="C://AuxileryThings//Push-to-prod//Mann.JPG" alt="" />
       <br />
       <br />
-      This page is for pushing into the production
+      <h1>This is my photo</h1>
+      <br />
+      <img width={400} src="/Mann.JPG" alt="ss" />
+      <br />
+      <br />
 
+      This page is for pushing into the production
     </div>
-  )
+  );
 }
 
 export default App
